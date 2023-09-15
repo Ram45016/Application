343aa76a1a0400b1a9f4ef654c22df2ba4418a23
Ramanathan S <104961250+Ram45016@users.noreply.github.com>
2023-09-15 16:18:43 +0530

    Add files via upload

---
+++ Day3/App.js
@@ -0,0 +1,12 @@
+import './assets/css/App.css';
+import LoginAndSignUp from './components/LoginAndSignUp';
+
+function App() {
+  return (
+    <div className="App">
+        <LoginAndSignUp/>
+    </div>
+  );
+}
+
+export default App;
